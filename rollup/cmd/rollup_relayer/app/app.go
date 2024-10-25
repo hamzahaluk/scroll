71f240b87cc8323ac3db37fd6041be5c92243277
jonastheis <4181434+jonastheis@users.noreply.github.com>
2024-10-25 15:51:53 +0800

    start implementation of restoring full previous state for relayer

--- rollup/cmd/rollup_relayer/app/app.go
+++ rollup/cmd/rollup_relayer/app/app.go
@@ -8,15 +8,17 @@ import (
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/scroll-tech/da-codec/encoding"
+	"github.com/scroll-tech/go-ethereum/common"
 	"github.com/scroll-tech/go-ethereum/ethclient"
 	"github.com/scroll-tech/go-ethereum/log"
+	"github.com/scroll-tech/go-ethereum/rollup/l1"
 	"github.com/urfave/cli/v2"
 
 	"scroll-tech/common/database"
 	"scroll-tech/common/observability"
 	"scroll-tech/common/utils"
 	"scroll-tech/common/version"
-
 	"scroll-tech/rollup/internal/config"
 	"scroll-tech/rollup/internal/controller/relayer"
 	"scroll-tech/rollup/internal/controller/watcher"
@@ -90,6 +92,16 @@ func action(ctx *cli.Context) error {
 
 	l2watcher := watcher.NewL2WatcherClient(subCtx, l2client, cfg.L2Config.Confirmations, cfg.L2Config.L2MessageQueueAddress, cfg.L2Config.WithdrawTrieRootSlot, genesis.Config, db, registry)
 
+	if cfg.RecoveryConfig.Enable {
+		log.Info("Starting rollup-relayer in recovery mode", "version", version.Version)
+
+		if err = restoreFullPreviousState(cfg, chunkProposer, batchProposer); err != nil {
+			log.Crit("failed to restore full previous state", "error", err)
+		}
+
+		return nil
+	}
+
 	// Watcher loop to fetch missing blocks
 	go utils.LoopWithContext(subCtx, 2*time.Second, func(ctx context.Context) {
 		number, loopErr := butils.GetLatestConfirmedBlockNumber(ctx, l2client, cfg.L2Config.Confirmations)
@@ -131,4 +143,171 @@ func Run() {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func restoreFullPreviousState(cfg *config.Config, chunkProposer *watcher.ChunkProposer, batchProposer *watcher.BatchProposer) error {
+	log.Info("Restoring full previous state with", "L1 block height", cfg.RecoveryConfig.L1BlockHeight, "latest finalized batch", cfg.RecoveryConfig.LatestFinalizedBatch)
+
+	// DB state should be clean: the latest batch in the DB should be finalized on L1. This function will
+	// restore all batches between the latest finalized batch in the DB and the latest finalized batch on L1.
+
+	// 1. Get latest finalized batch stored in DB
+	// 2. Get latest finalized L1 block
+	// 3. Get latest finalized batch from contract (at latest finalized L1 block)
+	// 4. Get batches one by one from stored in DB to latest finalized batch
+	//    - reproduce chunks / batches
+
+	// 1. Get latest finalized batch stored in DB
+	latestDBBatch, err := batchProposer.BatchORM().GetLatestBatch(context.Background())
+	fmt.Println("latestDBBatch", latestDBBatch)
+
+	// TODO:
+	//  1. what if it is a fresh start? -> latest batch is nil
+	//latestDBBatch.CommitTxHash
+
+	log.Info("Latest finalized batch in DB", "batch", latestDBBatch.Index, "hash", latestDBBatch.Hash)
+
+	// TODO: make these parameters -> part of genesis config?
+	scrollChainAddress := common.HexToAddress("0x2D567EcE699Eabe5afCd141eDB7A4f2D0D6ce8a0")
+	l1MessageQueueAddress := common.HexToAddress("0xF0B2293F5D834eAe920c6974D50957A1732de763")
+
+	l1Client, err := ethclient.Dial(cfg.L1Config.Endpoint)
+	if err != nil {
+		return fmt.Errorf("failed to connect to L1 client: %w", err)
+	}
+	reader, err := l1.NewReader(context.Background(), l1.Config{
+		ScrollChainAddress:    scrollChainAddress,
+		L1MessageQueueAddress: l1MessageQueueAddress,
+	}, l1Client)
+	if err != nil {
+		return fmt.Errorf("failed to create L1 reader: %w", err)
+	}
+
+	// 2. Get latest finalized L1 block
+	latestFinalizedL1Block, err := reader.GetLatestFinalizedBlockNumber()
+	if err != nil {
+		return fmt.Errorf("failed to get latest finalized L1 block number: %w", err)
+	}
+
+	log.Info("Latest finalized L1 block number", "latest finalized L1 block", latestFinalizedL1Block)
+
+	// 3. Get latest finalized batch from contract (at latest finalized L1 block)
+	latestFinalizedBatch, err := reader.LatestFinalizedBatch(latestFinalizedL1Block)
+	if err != nil {
+		return fmt.Errorf("failed to get latest finalized batch: %w", err)
+	}
+
+	log.Info("Latest finalized batch from L1 contract", "latest finalized batch", latestFinalizedBatch, "at latest finalized L1 block", latestFinalizedL1Block)
+
+	// 4. Get batches one by one from stored in DB to latest finalized batch.
+	// TODO: replace with this after testing: common.HexToHash(latestDBBatch.CommitTxHash)
+	hash := common.HexToHash("0x7c5ed6c542cbd5a2e59a3e9c35df49f16ff99d8dc01830f14c61d4b97c4f70f8")
+	receipt, err := l1Client.TransactionReceipt(context.Background(), hash)
+	if err != nil {
+		return fmt.Errorf("failed to get transaction receipt of latest DB batch finalization transaction: %w", err)
+	}
+	fromBlock := receipt.BlockNumber.Uint64()
+
+	log.Info("Fetching rollup events from L1", "from block", fromBlock, "to block", latestFinalizedL1Block, "from batch", latestDBBatch.Index, "to batch", latestFinalizedBatch)
+
+	batchEventsHeap := common.NewHeap[*batchEvents]()
+	commitEvents := common.NewShrinkingMap[uint64, *l1.CommitBatchEvent](1000)
+	var innerErr error
+	count := 0
+	err = reader.FetchRollupEventsInRangeWithCallback(fromBlock, latestFinalizedL1Block, func(event l1.RollupEvent) bool {
+		// We're only interested in batches that are newer than the latest finalized batch in the DB.
+		if event.BatchIndex().Uint64() <= latestDBBatch.Index {
+			return true
+		}
+
+		switch event.Type() {
+		case l1.CommitEventType:
+			commitEvent := event.(*l1.CommitBatchEvent)
+			commitEvents.Set(commitEvent.BatchIndex().Uint64(), commitEvent)
+
+		case l1.FinalizeEventType:
+			// TODO: this needs to be the same as in d
+			finalizeEvent := event.(*l1.FinalizeBatchEvent)
+			commitEvent, exists := commitEvents.Get(finalizeEvent.BatchIndex().Uint64())
+			if !exists {
+				innerErr = fmt.Errorf("commit event not found for finalize event %d", finalizeEvent.BatchIndex().Uint64())
+				return false
+			}
+
+			batchEventsHeap.Push(newBatchEvents(commitEvent, finalizeEvent))
+
+			// Stop fetching rollup events if we reached the latest finalized batch.
+			if finalizeEvent.BatchIndex().Uint64() >= latestFinalizedBatch {
+				return false
+			}
+
+			if count >= 100 {
+				return false
+			}
+			count++
+
+		case l1.RevertEventType:
+			// We ignore reverted batches.
+			commitEvents.Delete(event.BatchIndex().Uint64())
+		}
+
+		return true
+	})
+	if innerErr != nil {
+		return fmt.Errorf("failed to fetch rollup events (inner): %w", innerErr)
+	}
+	if err != nil {
+		return fmt.Errorf("failed to fetch rollup events: %w", err)
+	}
+
+	// 5. Process all finalized batches: fetch L2 blocks and reproduce chunks and batches.
+	for batchEventsHeap.Len() > 0 {
+		nextBatch := batchEventsHeap.Pop().Value()
+
+		// 5.1. Fetch commit tx data for batch (via commit event).
+		args, err := reader.FetchCommitTxData(nextBatch.commit)
+		if err != nil {
+			return fmt.Errorf("failed to fetch commit tx data: %w", err)
+		}
+
+		codec, err := encoding.CodecFromVersion(encoding.CodecVersion(args.Version))
+		if err != nil {
+			return fmt.Errorf("failed to get codec: %w", err)
+		}
+
+		daChunksRawTxs, err := codec.DecodeDAChunksRawTx(args.Chunks)
+		if err != nil {
+			return fmt.Errorf("failed to decode DA chunks: %w", err)
+		}
+		lastChunk := daChunksRawTxs[len(daChunksRawTxs)-1]
+		lastBlockInBatch := lastChunk.Blocks[len(lastChunk.Blocks)-1].Number()
+
+		log.Info("Last L2 block in batch", "batch", nextBatch.commit.BatchIndex(), "L2 block", lastBlockInBatch)
+
+		// 5.2. Fetch L2 blocks for each chunk.
+		// re-create encoding.Chunk and call InsertChunk
+		// same with encoding.Batch and call InsertBatch
+	}
+
+	return nil
+}
+
+type batchEvents struct {
+	commit   *l1.CommitBatchEvent
+	finalize *l1.FinalizeBatchEvent
+}
+
+func newBatchEvents(commit *l1.CommitBatchEvent, finalize *l1.FinalizeBatchEvent) *batchEvents {
+	if commit.BatchIndex().Uint64() != finalize.BatchIndex().Uint64() {
+		panic("commit and finalize batch indexes do not match")
+	}
+
+	return &batchEvents{
+		commit:   commit,
+		finalize: finalize,
+	}
+}
+
+func (e *batchEvents) CompareTo(other *batchEvents) int {
+	return e.commit.BatchIndex().Cmp(other.commit.BatchIndex())
 }
